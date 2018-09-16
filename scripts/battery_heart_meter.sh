fdb786a689494dd32ea57fc790f874e5dbc2c25f
Nicholas Ireland <nick@nicholasireland.ca>
2018-09-16 11:46:41 -0400

    will this work

--- scripts/battery_heart_meter.sh
+++ scripts/battery_heart_meter.sh
@@ -4,15 +4,15 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 battery_heart_meter() {
-    local red=$(get_tmux_option @heart_color "#[fg=$color_red]")
-    local black=$(get_tmux_option @black_heart_color "#[fg=$color_black]")
+    local red=$(get_tmux_option @heart_color "#[fg=red]")
+    local black=$(get_tmux_option @black_heart_color "#[fg=black]")
     
     local heart="$(echo -e '\u2665')"
 
     percent="$(pmset -g batt | grep -o '[0-9]\{1,3\}%')"
     percent=$(echo "$percent" | grep -o '[0-9]\{1,3\}')
 
-    remaining=$[$[$percent % 10]/2]
+    remaining=$[$[$[$percent / 100] * 5] % 1]
     used=$[5 - $remaining]
 
     left=""
@@ -24,7 +24,7 @@ battery_heart_meter() {
     for i in `seq 1 $used`; do
         right="$right $heart";
     done
-    echo "$red" "$right"
+    printf "$red$left$black$right"
 }
 
 battery_heart_meter
