8b41d9a68eb858074ad93efc67a0ce48f154c347
Nicholas Ireland <nick@nicholasireland.ca>
2018-09-27 13:04:11 -0400

    update ability to set heart icon

--- scripts/battery_heart_meter.sh
+++ scripts/battery_heart_meter.sh
@@ -7,7 +7,8 @@ battery_heart_meter() {
     local red=$(get_tmux_option @heart_color "#[fg=red]")
     local black=$(get_tmux_option @black_heart_color "#[fg=black]")
     
-    local heart="$(echo -e '\u2665')"
+    local heart_icon="$(echo -e '\u2665')"
+    local heart=$(get_tmux_option @heart_icon $heart_icon)
 
     percent="$(pmset -g batt | grep -o '[0-9]\{1,3\}%')"
     percent=$(echo "$percent" | grep -o '[0-9]\{1,3\}')
