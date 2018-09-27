2434366b48641ab3f6cad966c4e1f477f8fd3188
Nicholas Ireland <nick@nicholasireland.ca>
2018-09-27 13:14:20 -0400

    again

--- scripts/battery_heart_meter.sh
+++ scripts/battery_heart_meter.sh
@@ -8,7 +8,8 @@ battery_heart_meter() {
     local black=$(get_tmux_option @black_heart_color "#[fg=black]")
     
     local heart_icon="$(echo -e '\u2665')"
-    local heart=$(get_tmux_option "@heart_icon" "$heart_icon")
+    unset heart
+    local heart="$(get_tmux_option "@heart_icon" $heart_icon)"
 
     percent="$(pmset -g batt | grep -o '[0-9]\{1,3\}%')"
     percent=$(echo "$percent" | grep -o '[0-9]\{1,3\}')
